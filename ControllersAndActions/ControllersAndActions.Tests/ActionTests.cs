a91e91b9813c837914485d4e398fbd2d1b4e8ef9
declan.deng@outlook.com <declan.deng@outlook.com>
2020-12-20 15:47:14 +1100

    Pass data from an action method to a view & Perform redirections

---
+++ ControllersAndActions/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,57 @@
+﻿using ControllersAndActions.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            // Arrange - create the controller
+            ExampleController target = new ExampleController();
+
+            // Act - call the action method
+            ViewResult result = target.Index();
+
+            // Assert - check the result
+            Assert.AreEqual("Index", result.ViewName);
+            Assert.AreEqual("Hello", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void RedirectTest()
+        {
+            // Arrange - create the controller
+            ExampleController target = new ExampleController();
+
+            // Act - call the action method
+            //RedirectResult result = target.Redirect();
+            RedirectToRouteResult result = target.Redirect();
+
+            // Assert - check the result
+            Assert.IsFalse(result.Permanent);
+            //Assert.AreEqual("/Example/Index", result.Url);
+            Assert.AreEqual("Example", result.RouteValues["controller"]);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("MyID", result.RouteValues["ID"]);
+        }
+
+        [TestMethod]
+        public void StatusCodeResultTest()
+        {
+            // Arrange - create the controller
+            ExampleController target = new ExampleController();
+
+            // Act - call the action method
+            HttpStatusCodeResult result = target.StatusCode();
+
+            // Assert - check the result
+            Assert.AreEqual(404, result.StatusCode);
+            Assert.AreEqual("URL cannot be serviced", result.StatusDescription);
+        }
+    }
+}
---
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index()
+        {
+            if (TempData["Message"] != null && TempData["Date"] != null)
+            {
+                ViewBag.Message = TempData["Message"];
+                ViewBag.Date = TempData["Date"];
+            }
+            else
+            {
+                ViewBag.Message = "Hello";
+                ViewBag.Date = DateTime.Now;
+            }
+
+            return View("Index");
+        }
+
+        public RedirectToRouteResult Redirect()
+        {
+            //return Redirect("/Example/Index");
+            return RedirectToRoute(new
+            {
+                controller = "Example",
+                action = "Index",
+                ID = "MyID"
+            });
+        }
+
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            TempData["Message"] = "Hello";
+            TempData["Date"] = DateTime.Now;
+            //return RedirectToAction("Index", "Basic");
+            return RedirectToAction("Index");
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            //return HttpNotFound();
+            //return new HttpUnauthorizedResult();
+            return new HttpStatusCodeResult(404, "URL cannot be serviced");
+        }
+    }
+}
