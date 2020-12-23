0d6ba34985cae3d78c53e851d3422f5498fc2572
declan.deng@outlook.com <declan.deng@outlook.com>
2020-12-23 11:24:45 +1100

    Improve performance by asynchronous controllers

---
+++ ControllerExtensibility/ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -0,0 +1,37 @@
+﻿using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    //public class RemoteDataController : Controller
+    //{
+    //    public ActionResult Data()
+    //    {
+    //        RemoteService service = new RemoteService();
+    //        string data = service.GetRemoteData();
+    //        return View((object)data);
+    //    }
+    //}
+    public class RemoteDataController : AsyncController
+    {
+        public async Task<ActionResult> Data()
+        {
+            string data = await Task<string>.Factory.StartNew(() =>
+            {
+                return new RemoteService().GetRemoteData();
+            });
+            return View((object)data);
+        }
+
+        public async Task<ActionResult> ConsumeAsyncMethod()
+        {
+            string data = await new RemoteService().GetRemoteDataAsync();
+            return View("Data", (object)data);
+        }
+    }
+}
