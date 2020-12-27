3892d9207dbeb65eb476ec07f58d803a2ff1310d
declan.deng@outlook.com <declan.deng@outlook.com>
2020-12-27 22:07:51 +1100

    Add some examples of action results & action parameters

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -9,11 +9,36 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ActionResult ActionResultExample() 
+        {
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return Content("Hello World!");
+        }
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek"};
             return View(movie);
+            
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
